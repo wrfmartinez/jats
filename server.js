166a0b91481baf1ac9007155946371dd75c88b00
wrfmartinez <wrmartinez709@gmail.com>
2024-05-01 14:21:00 -0400

    Add route to dashboard

--- server.js
+++ server.js
@@ -7,8 +7,15 @@ const app = express();
 dotenv.config();
 
 // ROUTES
+
+// render landing page
 app.get('/', (req, res) => {
   res.render('index.ejs');
+})
+
+// render dashboard
+app.get('/dashboard', async (req, res) => {
+  res.send("Hi dash");
 })
 
 const PORT = 3000;
