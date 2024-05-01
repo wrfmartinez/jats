43542726ab789d24c6bff76bcc1714754ac815e3
wrfmartinez <wrmartinez709@gmail.com>
2024-05-01 14:46:26 -0400

    Create user schema

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  desiredPosition: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipcode: {
+    type: String,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('User', userSchema);
