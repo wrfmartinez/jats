4d9348f53118343addfac1ffb0ac60eb80c3d745
wrfmartinez <wrmartinez709@gmail.com>
2024-05-01 14:35:09 -0400

    Add express configurations and connect to MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const path = require('path');
 
-// CONFIGURATIONS
+/* CONFIGURATIONS */
 const app = express();
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
 dotenv.config();
 
-// ROUTES
+// MONGODB
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on(`connected`, () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+})
 
+/* ROUTES*/
 // render landing page
 app.get('/', (req, res) => {
   res.render('index.ejs');
@@ -17,6 +25,8 @@ app.get('/', (req, res) => {
 app.get('/dashboard', async (req, res) => {
   res.render('dashboard.ejs');
 })
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
