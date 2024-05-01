fd5283f6344b3c238276e726b2bff86bf02756a3
wrfmartinez <wrmartinez709@gmail.com>
2024-05-01 14:56:04 -0400

    Create application schema

---
+++ models/application.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const applicationSchema = mongoose.Schema({
+  companyName: {
+    type: String,
+    required: true,
+  },
+  positionTitle: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  jobRequirements: {
+    type: String,
+    required: false,
+  },
+  notes: {
+    type: String,
+    required: false,
+  }
+})
+
+module.exports = mongoose.model('Application', applicationSchema);
