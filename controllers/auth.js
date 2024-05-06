381bee6fe3f62f472fbe0626e7f27c18bb299670
wrfmartinez <wrmartinez709@gmail.com>
2024-05-06 10:44:30 -0400

    Clean code for better readability

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,12 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
-// render create account form
 router.get('/sign-up', (req, res) => {
   res.render('auth/sign-up.ejs');
 })
-// render signin account form
 router.get('/sign-in', (req, res) => {
   res.render('auth/sign-in.ejs');
 })
-// signs user out
 router.get('/sign-out', (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
@@ -34,19 +31,23 @@ router.post('/sign-up', async (req, res) => {
   const user = await User.create(req.body);
   res.redirect('/');
 })
-// sign user in
+// authenticate user credentials and sign user in
 router.post('/sign-in', async (req, res) => {
   const userInDatabase = await User.findOne({ email: req.body.email });
+
   if (!userInDatabase) {
     return res.send("Login failed. Please try again.");
   }
+
   const validPassword = bcrypt.compareSync(
     req.body.password,
     userInDatabase.password
   );
+
   if (!validPassword) {
     return res.send("Login failed. Please try again.");
   }
+
   req.session.user = {
     email: userInDatabase.email,
     firstName: userInDatabase.firstName,
@@ -54,6 +55,7 @@ router.post('/sign-in', async (req, res) => {
     desiredPosition: userInDatabase.desiredPosition,
     location: userInDatabase.location,
   }
+
   req.session.save(() => {
     res.redirect('/dashboard');
   })
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 dotenv.config();
 // middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
-// automatically manages session data for each user request
+// automatically manages session data for each user request and stores them in the database
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -39,6 +39,7 @@ mongoose.connection.on(`connected`, () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 })
 
+// Quote Garden API fetch request
 const getQuote = async () => {
   const QUOTE_URL = "https://quote-garden.onrender.com/api/v3/quotes/random";
   const res = await fetch(QUOTE_URL, ["work"]);
@@ -47,47 +48,40 @@ const getQuote = async () => {
 }
 
 /* ROUTES*/
-// render landing page
 app.get('/', async (req, res) => {
   res.render('index.ejs');
 })
-// render dashboard
+// render dashboard and pass applications that are related to the specific user that is logged in
+// calls the getQuote API function then passes it to the dashboard view
 app.get('/dashboard', async (req, res) => {
-  let randomQuote = null;
+  const randomQuote = await getQuote();
   const applications = await Application.find({ createdBy: req.session.user.email });
-  randomQuote = await getQuote();
   res.render('dashboard.ejs', { applications: applications, randomQuote: randomQuote });
 })
-// render applications page
+// render applications page and locates specific applications that are associated with the logged in user
 app.get('/application', async (req, res) => {
   const applications = await Application.find({ createdBy: req.session.user.email });
   res.render('./applications/index.ejs', { applications: applications });
 })
-// render create applications page
 app.get('/application/new', (req, res) => {
   res.render('./applications/new.ejs');
 })
-// view a specific application
 app.get('/application/:applicationId', async (req, res) => {
   const foundApplication = await Application.findById(req.params.applicationId);
   res.render('./applications/show.ejs', { application: foundApplication });
 })
-// show edit application page
 app.get('/application/:applicationId/edit', async (req, res) => {
   const applicationToEdit = await Application.findById(req.params.applicationId);
   res.render('./applications/edit.ejs', { application: applicationToEdit })
 })
-// create a new application entry
 app.post('/application/new', async (req, res) => {
   const application = await Application.create(req.body);
   res.redirect('/application');
 })
-// edit an application
 app.put('/application/:applicationId/edit', async (req, res) => {
   const applicationToEdit = await Application.findByIdAndUpdate(req.params.applicationId, req.body);
   res.redirect(`/application/${req.params.applicationId}`);
 })
-// delete an application
 app.delete('/application/:applicationId', async (req, res) => {
   const application = await Application.findByIdAndDelete(req.params.applicationId);
   res.redirect('/application');
